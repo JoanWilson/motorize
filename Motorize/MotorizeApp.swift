0a45a0ad33b44baf748ba027e6b7b94f7d38a73c
Joan Wilson <joanwmoliveira@gmail.com>
2023-03-23 17:19:13 -0300

    Initial Commit

---
+++ Motorize/MotorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotorizeApp.swift
+//  Motorize
+//
+//  Created by Joan Wilson Oliveira on 23/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MotorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
